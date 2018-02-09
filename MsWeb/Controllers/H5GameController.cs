d37dd32de479f0496e6b41b935017c383917995c
Lisx <git>
2018-02-09 08:58:06 +0800

    增加H5小游戏，删除微信服务平台和积分平台

---
+++ MsWeb/Controllers/H5GameController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MsWeb.Controllers
+{
+    public class H5GameController : Controller
+    {
+        // GET: H5Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
